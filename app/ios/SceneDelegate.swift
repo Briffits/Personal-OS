3bd6cff383f65b2307f11bda85a736f741f382e9
Mark Briffa <330034320+Briffits@users.noreply.github.com>
2026-09-22 10:39:01 +0200

    Add React Native iOS baseline

---
+++ app/ios/SceneDelegate.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = scene as? UIWindowScene else {
+      return
+    }
+
+    guard
+      let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+      let reactNativeFactory = appDelegate.reactNativeFactory
+    else {
+      return
+    }
+
+    let window = UIWindow(windowScene: windowScene)
+    self.window = window
+
+    reactNativeFactory.startReactNative(
+      withModuleName: "PersonalOS",
+      in: window,
+      launchOptions: appDelegate.launchOptions
+    )
+  }
+}
